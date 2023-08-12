bc224804a64bebc10c4d4270411e1adf45d746c4
7yes <100316346+7yes@users.noreply.github.com>
2023-08-12 16:16:55 -0500

    binding

--- app/src/main/java/com/example/c23/MainActivity.kt
+++ app/src/main/java/com/example/c23/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.c23
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.c23.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnBroadcast.setOnClickListener {
+            //
+        }
+
+        binding.btnNotification.setOnClickListener {
+            //
+        }
     }
 }
