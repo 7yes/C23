38a76e3d0c3b23a4db4b3f43373caefbe4be11c1
7yes <100316346+7yes@users.noreply.github.com>
2023-08-13 12:18:40 -0500

    set up notification

--- app/src/main/java/com/example/c23/MainActivity.kt
+++ app/src/main/java/com/example/c23/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.c23
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
 import com.example.c23.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +14,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
 
+    override fun onStart() {
+        super.onStart()
+        notificationFu()
+    }
+    private fun notificationFu() {
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager //sirve se agrega postpermission en manifest
+//  val notificationManager= NotificationManagerCompat.from(this) // sirve hay q add permission check
+        val channelComm =
+            NotificationChannel("MiId", "NOmbre del canal", NotificationManager.IMPORTANCE_HIGH)
+        channelComm.description = "es el mensaje"
+        notificationManager.createNotificationChannel(channelComm)
 
+        val notification = NotificationCompat.Builder(this, "MiId")
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setSmallIcon(androidx.core.R.drawable.ic_call_decline)
+            .setContentTitle("Es el Titulo")
+            .setContentText("Es el mensaje de bla bla bla bla bla bla bla")
+
+        notificationManager.notify(1, notification.build())
     }
 }
