fe7f432a43139c6ac09afd27e2abb8ef28914938
7yes <100316346+7yes@users.noreply.github.com>
2023-08-14 16:42:04 -0500

    initRV, update adapter

--- app/src/main/java/com/example/c23/MainActivity.kt
+++ app/src/main/java/com/example/c23/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.c23
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.c23.data.RecetasAdater
 import com.example.c23.data.RecetasApi
+import com.example.c23.data.model.RecetasResponseItem
 import com.example.c23.databinding.ActivityMainBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -13,10 +16,19 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var adater: RecetasAdater
+    private var recetasList = mutableListOf<RecetasResponseItem>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initRV()
+    }
+
+    private fun initRV() {
+        adater = RecetasAdater(recetasList)
+        binding.rvRecetas.layoutManager = LinearLayoutManager(this)
+        binding.rvRecetas.adapter = adater
     }
 
     override fun onStart() {
@@ -24,15 +36,20 @@ class MainActivity : AppCompatActivity() {
         getRecetas()
     }
 
-    fun getRecetas(){
+    fun getRecetas() {
         CoroutineScope(Dispatchers.IO).launch {
             val call = getRetrofit().create(RecetasApi::class.java).getAllRecetas()
-            if (call.isSuccessful){
-                val body =call.body()
+            if (call.isSuccessful) {
+                val body = call.body()
                 Log.d("TAG", "getRecetas: $body ")
 
-            }
-            else{
+                body?.let {
+                    recetasList.clear()
+                    recetasList.addAll(it)
+                    runOnUiThread { adater.notifyDataSetChanged() }
+                }
+
+            } else {
                 showError()
             }
         }
@@ -42,7 +59,7 @@ class MainActivity : AppCompatActivity() {
         Log.d("TAG", "showError Error")
     }
 
-    fun getRetrofit():Retrofit{
+    fun getRetrofit(): Retrofit {
         return Retrofit.Builder()
             .baseUrl("https://demo3038897.mockable.io/")
             .addConverterFactory(GsonConverterFactory.create())
