39851ee677ad1112755d3d7bbf8c1074928b1d5e
7yes <100316346+7yes@users.noreply.github.com>
2023-08-24 15:54:10 -0500

    shareSheet

--- app/src/main/java/com/example/c23/MainActivity.kt
+++ app/src/main/java/com/example/c23/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.c23
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.c23.databinding.ActivityMainBinding
@@ -11,6 +13,28 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.btnShare.setOnClickListener { shareImage()} // llama a una de las 2 share fun,
+    }
 
+    private fun share() {
+        val intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT,"Whatever")
+            type = "text/plain"
+            putExtra(Intent.EXTRA_TITLE,"E Title")
+        }
+        val shareIntent = Intent.createChooser(intent,null)
+        startActivity(shareIntent)
+    }
+
+    private fun shareImage() {
+        val intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_STREAM, Uri.EMPTY)
+            type = "image/jpeg"
+            putExtra(Intent.EXTRA_TITLE,"E Title")
+        }
+        val shareIntent = Intent.createChooser(intent,null)
+        startActivity(shareIntent)
     }
 }
