061e748bc71a0dc08b91df772717b5ac681655d9
7yes <100316346+7yes@users.noreply.github.com>
2023-08-31 23:26:50 -0500

    UI pomo

--- app/src/main/java/com/example/c23/MainActivity.kt
+++ app/src/main/java/com/example/c23/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.c23
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.c23.databinding.ActivityMainBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -10,7 +13,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+binding.btn.setOnClickListener {  }
 
+    }
+}
 
+val counter: Flow<Int> = flow {
+    var bombitas = 1
+    while (true) {
+        bombitas += 1
+        emit(bombitas)
+        delay(1000)
     }
 }
